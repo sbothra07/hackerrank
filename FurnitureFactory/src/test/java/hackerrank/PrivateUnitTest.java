90346809d6d52a129ada680e908d0d855a1712a6
sbothra07 <88421342+sbothra07@users.noreply.github.com>
2021-11-26 20:37:58 +0530

    Add files via upload

--- FurnitureFactory/src/test/java/hackerrank/PrivateUnitTest.java
+++ FurnitureFactory/src/test/java/hackerrank/PrivateUnitTest.java
@@ -1,16 +1,14 @@
 package hackerrank;
 
-import java.util.HashMap;
-
 import static junit.framework.TestCase.assertEquals;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
-public class PrivateUnitTest {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class SampleUnitTest {
     private static FurnitureOrder furnitureFactory;
 
     @BeforeClass
@@ -19,72 +17,40 @@ public class PrivateUnitTest {
     }
 
     @Test
-    public void _08_orderNothing() {
-        furnitureFactory.addToOrder(Furniture.TABLE, 0);
-        furnitureFactory.addToOrder(Furniture.CHAIR, 0);
-        furnitureFactory.addToOrder(Furniture.COUCH, 0);
-
-        assertEquals(0.0f, furnitureFactory.getTotalOrderCost());
+    public void _01_getChairCount() {
+        assertEquals(0, furnitureFactory.getTypeCount(Furniture.CHAIR));
     }
 
     @Test
-    public void _09_placeOrders() {
+    public void _02_orderFourChairs() {
+        furnitureFactory.addToOrder(Furniture.CHAIR, 4);
+        assertEquals(4, furnitureFactory.getTypeCount(Furniture.CHAIR));
+    }
+
+    @Test
+    public void _03_orderThreeCouches() {
+        furnitureFactory.addToOrder(Furniture.COUCH, 3);
+        assertEquals(3, furnitureFactory.getTypeCount(Furniture.COUCH));
+    }
+
+    @Test
+    public void _04_orderedChairsCost() {
+        assertEquals(400.0f, furnitureFactory.getTypeCost(Furniture.CHAIR));
+    }
+
+    @Test
+    public void _05_orderedTablesCost() {
+        assertEquals(0.0f, furnitureFactory.getTypeCost(Furniture.TABLE));
+    }
+
+    @Test
+    public void _06_orderedCouchesCost() {
+        assertEquals(1500.0f, furnitureFactory.getTypeCost(Furniture.COUCH));
+    }
+
+    @Test
+    public void _07_totalOrderCost() {
         furnitureFactory.addToOrder(Furniture.TABLE, 6);
-        furnitureFactory.addToOrder(Furniture.CHAIR, 10);
-        furnitureFactory.addToOrder(Furniture.COUCH, 5);
-
-        assertEquals(5000.0f, furnitureFactory.getTotalOrderCost());
-    }
-
-    @Test
-    public void _10_validateFurnitureCostAndQuantity() {
-//        ArrayList<Furniture> orderedFurniture = furnitureFactory.getOrderedFurniture();
-        HashMap<Furniture, Integer> orderedFurniture = furnitureFactory.getOrderedFurniture();
-
-        assertEquals(21, orderedFurniture.values().stream().mapToInt(Integer::intValue).sum());
-
-        orderedFurniture.keySet().forEach(furniture -> {
-            if ("Chair".equals(furniture.label())) {
-                assertEquals(100.0f, furniture.cost());
-            }
-
-            if ("Table".equals(furniture.label())) {
-                assertEquals(250.0f, furniture.cost());
-            }
-
-            if ("Couch".equals(furniture.label())) {
-                assertEquals(500.0f, furniture.cost());
-            }
-        });
-
-        assertEquals(10, furnitureFactory.getTypeCount(Furniture.CHAIR));
-        assertEquals(1000.0f, furnitureFactory.getTypeCost(Furniture.CHAIR));
-
-        assertEquals(6, furnitureFactory.getTypeCount(Furniture.TABLE));
-        assertEquals(1500.0f, furnitureFactory.getTypeCost(Furniture.TABLE));
-
-        assertEquals(5, furnitureFactory.getTypeCount(Furniture.COUCH));
-
-        assertEquals(2500.0f, furnitureFactory.getTypeCost(Furniture.COUCH));
-
-        // Validates order size
-        assertEquals(21, furnitureFactory.getTotalOrderQuantity());
-    }
-
-    @Test
-    public void _11_validateFurniture() {
-    	for (Furniture f : Furniture.values()) {
-    		switch(f.label()) {
-    		case("Chair"):
-    			assertEquals(100.0f, f.cost());
-    			break;
-    		case("Table"):
-    			assertEquals(250.0f, f.cost());
-    			break;
-    		case("Couch"):
-    			assertEquals(500.0f, f.cost());
-    			break;
-    		}
-    	}
+        assertEquals(3400.0f, furnitureFactory.getTotalOrderCost());
     }
 }
